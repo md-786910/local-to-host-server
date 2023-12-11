e15471184b7814ceaebc0ec5bdd0def18c6d432f
md ashif reza <76996887+md-786910@users.noreply.github.com>
2023-12-11 22:32:19 +0530

    fix issues

---
+++ index.js
@@ -0,0 +1,47 @@
+const app = require("express")();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+const url = require("url");
+const bodyParser = require("body-parser");
+
+app.use(bodyParser());
+
+let clientResponseRef;
+
+app.get("/*", (req, res) => {
+  const pathname = url.parse(req.url).pathname;
+
+  const obj = {
+    pathname: pathname,
+    method: "get",
+    params: req.query,
+  };
+
+  io.emit("page-request", obj);
+  clientResponseRef = res;
+});
+
+app.post("/*", (req, res) => {
+  const pathname = url.parse(req.url).pathname;
+
+  const obj = {
+    pathname: pathname,
+    method: "post",
+    params: req.body,
+  };
+
+  io.emit("page-request", obj);
+  clientResponseRef = res;
+});
+
+io.on("connection", (socket) => {
+  console.log("a node connected");
+  socket.on("page-response", (response) => {
+    clientResponseRef.send(response);
+  });
+});
+
+const server_port = process.env.YOUR_PORT || process.env.PORT || 3000;
+http.listen(server_port, () => {
+  console.log("listening on *:" + server_port);
+});
